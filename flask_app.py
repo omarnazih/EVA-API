023c88b09d4de2e34be93440855a2371909a37ac
omarnazih <omarnazeh29@gmail.com>
2020-06-13 19:26:27 +0200

    adding the model

--- flask_app.py
+++ flask_app.py
@@ -1,12 +1,18 @@
-from werkzeug.wrappers import Request, Response
-from flask import Flask
+# import the necessary packages
+from keras.preprocessing.image import img_to_array
+from keras.applications import imagenet_utils
+from PIL import Image
+import numpy as np
+import flask
+import io
 
-app = Flask(__name__)
+# initialize our Flask application and the Keras model
+app = flask.Flask(__name__)
+model = None
 
-@app.route("/")
-def hello():
-    return "Hello World!"
-
-if __name__ == '__main__':
-    from werkzeug.serving import run_simple
-    run_simple('localhost', 9000, app)
+def load_model():
+    # load the pre-trained Keras model (here we are using a model
+    # pre-trained on ImageNet and provided by Keras, but you can
+    # substitute in your own networks just as easily)
+    global model
+    model = load_model('EVA model/fer2013_mini_XCEPTION.119-0.65.hdf5')
